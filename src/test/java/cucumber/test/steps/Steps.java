b064aeb83c6307df2d21579344a2e3a35f07bbe6
Pavel Anihimovsky <p.anihimovsky@a1qa.com>
2019-12-27 17:13:28 +0300

    Add selenium lib

--- src/test/java/cucumber/test/steps/Steps.java
+++ src/test/java/cucumber/test/steps/Steps.java
@@ -1,11 +1,13 @@
 package cucumber.test.steps;
 
+import aquality.selenium.browser.BrowserManager;
 import cucumber.api.java.en.Given;
 
 public class Steps {
 
     @Given("first step")
     public void testGiven() {
+        BrowserManager.getBrowser().goTo("https://www.google.com/");
         System.out.println("First step");
     }
 }
